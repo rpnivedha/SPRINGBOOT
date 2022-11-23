22c7f5787352ce7b30e92ce895191447f18442e4
rpnivedha <rpnivedha16@gmail.com>
2022-11-23 23:20:11 +0530

    Initial commit

---
+++ userservicespringboot-main/user-service/src/main/java/net/javaguides/userservice/UserServiceApplication.java
@@ -0,0 +1,32 @@
+// package net.javaguides.userservice;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class UserServiceApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(UserServiceApplication.class, args);
+// 	}
+
+// }
+package net.javaguides.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
